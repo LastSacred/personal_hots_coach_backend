1a3efb2f7bf881c06d05042c15a5eb713de03c83
Arthur Torres <arthurtorres002@gmail.com>
2019-02-19 08:39:19 -0500

    add Hero.import

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -3,4 +3,21 @@ class Hero < ApplicationRecord
   has_many :roster_listings
   has_many :matches, through: :hero_picks
   has_many :users, through: :roster_listings
+
+  before_validation :format_role
+
+  def self.import
+    Adapter.get("heroes").each do |hero|
+      self.find_or_create_by(name: hero["name"]).update(role: hero["role"])
+    end
+  end
+
+  private
+
+  def format_role
+    if self.role
+      self.role = self.role.upcase[0..3]
+    end
+  end
+
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -2,8 +2,8 @@ class Map < ApplicationRecord
   has_many :matches
 
   def self.import
-    Adapter.get("maps").collect do |map|
-      self.find_or_create_by(name: map["name"]).name
+    Adapter.get("maps").each do |map|
+      new_map = self.find_or_create_by(name: map["name"])
     end
   end
 
