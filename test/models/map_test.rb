fca983b977b1fb3c2e93e07fe7db569da9c31558
Arthur Torres <arthurtorres002@gmail.com>
2019-02-18 21:41:24 -0500

    create test for Map.import

--- test/models/map_test.rb
+++ test/models/map_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class MapTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test ".import" do
+    Map.import
+
+    map_names = Map.all.collect { |map| map.name }
+
+    assert_includes(map_names, "Braxis Holdout")
+  end
 end
