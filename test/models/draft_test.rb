c4e1316a0f10e4525f8814ada4d77af88c1e8141
Arthur Torres <arthurtorres002@gmail.com>
2019-02-28 09:27:37 -0500

    draft tests

--- test/models/draft_test.rb
+++ test/models/draft_test.rb
@@ -1,13 +1,14 @@
 require 'test_helper'
 
-class Draft < ActiveSupport::TestCase
-  # This isn't working
-  # test ".initialize" do
-  #   byebug
-  #   draft = Draft.new(@@stub_draft_props)
-  #
-  #   assert_equal 4, draft.bans.count
-  #   assert_equal 2, draft.with_heroes.count
-  #   assert_equal 3, draft.against_heroes.count
-  # end
+class DraftTest < ActiveSupport::TestCase
+  test ".initialize" do
+    alt = Map.find_by(name: "Alterac Pass")
+    draft = Draft.new(@@stub_draft_props)
+
+    assert_equal "LastSacred", draft.user
+    assert_equal alt, draft.map
+    assert_equal 4, draft.bans.count
+    assert_equal 2, draft.with_heroes.count
+    assert_equal 3, draft.against_heroes.count
+  end
 end
