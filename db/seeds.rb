8e4dd5f82fcfbf411174a374b0071154bb7bec7d
Arthur Torres <arthurtorres002@gmail.com>
2019-02-28 13:21:21 -0500

    user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lastsacred = User.create(name: "LastSacred", battletag: "LastSacred#1265", replay_path: "./test/replays/", auto_roster: false)
+
+Match.import(lastsacred.replay_path)
+
+hero_names = [
+  "Lúcio",
+  "Probius",
+  "Sonya",
+  "Artanis",
+  "Kharazim",
+  "Zul'jin",
+  "Stitches",
+  "Diablo",
+  "Samuro",
+  "Kael'thas",
+  "Mal'Ganis",
+  "Varian",
+  "Greymane",
+  "Li Li",
+  "Tracer",
+  "Azmodan"
+]
+
+heroes = hero_names.collect do |hero_name|
+  Hero.find_by(name: hero_name)
+end
+
+lastsacred.update(heroes: heroes)
