35ee4d9842df62b699ede48985cfddf6362c042a
Arthur Torres <arthurtorres002@gmail.com>
2019-03-11 12:03:53 -0400

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  private
+
+  def current_user
+    token = request.headers['Access-Token']
+    reutrn nil unless token
+    payload = JWT.decode(token, ENV['SECRET']).first
+    User.find(payload['userId'])
+  end
+
+  def authorize!
+    render json: {error: "Unauthorized"}, status: :unauthorized unless current_user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authorize!, only: [:update]
 
   def create
      @user = User.new(user_params)
