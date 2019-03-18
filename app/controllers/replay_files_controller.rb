3df4c6bf46d7800600470f7204210a9f921ce0bc
Arthur Torres <arthurtorres002@gmail.com>
2019-03-18 16:22:51 -0400

    in progress

--- app/controllers/replay_files_controller.rb
+++ app/controllers/replay_files_controller.rb
@@ -2,24 +2,29 @@ class ReplayFilesController < ApplicationController
   before_action :authorize!, only: [:create]
 
   def create
-    @file_name = replay_file_params.original_filename
-    @file = replay_file_params.tempfile
+    @replay_files = replay_file_params.collect do |file|
+      @file_name = file.original_filename
+      @file = file.tempfile
 
-    match = Adapter.post_replay(@file)
-    replay_id = (match["status"] == "AiDetected" ? nil : match["id"])
+      match = Adapter.post_replay(@file)
+      replay_id = (match["status"] == "AiDetected" ? nil : match["id"])
 
-    @replay_file = ReplayFile.find_or_create_by(
-      name: @file_name,
-      user: current_user,
-      match: Match.find_or_create_by(replay_id: replay_id)
-    )
+      @replay_file = ReplayFile.find_or_create_by(
+        name: @file_name,
+        user: current_user,
+        match: Match.find_or_create_by(replay_id: replay_id)
+      )
 
-    render json: @replay_file.to_json, status: :created
+      sleep(1.5)
+      @replay_file
+    end
+
+    render json: @replay_files.to_json, status: :create
   end
 
   private
 
   def replay_file_params
-    params.require(:file)
+    params.require(:files)
   end
 end
