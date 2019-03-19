ea32535b270cf8c2b4a83bea45da3310100f56be
Arthur Torres <arthurtorres002@gmail.com>
2019-03-19 11:34:10 -0400

    matches#create action test

--- test/controllers/matches_controller_test.rb
+++ test/controllers/matches_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class MatchesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#create' do
+    bob = User.find_by(name: "BobRoss")
+    token = JWT.encode({userId: bob.id}, ENV['SECRET'])
+
+    post matches_url, headers:{
+      "Access-Token": token
+      }
+
+    assert_response :accepted
+  end
 end
