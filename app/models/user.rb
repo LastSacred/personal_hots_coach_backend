3686c7317e710eec8666b5f4c06340b5e64e1e36
Arthur Torres <arthurtorres002@gmail.com>
2019-04-25 14:30:07 -0400

    refactor User#picklist to use Scenario, remove User#score and all helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,53 +42,9 @@ class User < ApplicationRecord
     return self.heroes
   end
 
-  def score(**params)
-    matches = matches_as(params[:as_hero]) unless params[:draft]
-    matches = filter_by_map(matches, params[:map]) if params[:map]
-    matches = filter_by_hero(matches, :with, params[:with_hero]) if params[:with_hero]
-    matches = filter_by_hero(matches, :against, params[:against_hero]) if params[:against_hero]
-
-    if params[:draft]
-      scores = []
-
-      params[:draft][:with_heroes].each do |hero|
-        scores << self.score(as_hero: params[:as_hero], with_hero: hero, map: params[:draft][:map])
-      end
-
-      params[:draft][:against_heroes].each do |hero|
-        scores << self.score(as_hero: params[:as_hero], against_hero: hero, map: params[:draft][:map])
-      end
-    else
-      scores = matches.collect do |match|
-        my_pick(match).win ? 1000 : 0
-      end
-    end
-
-    if params[:draft]
-      min = 4
-      fill = self.score(as_hero: params[:as_hero], map: params[:draft][:map])
-    elsif params[:map] && (params[:with_hero] || params[:against_hero])
-      min = 5
-      fill = self.score(as_hero: params[:as_hero], with_hero: params[:with_hero]) if params[:with_hero]
-      fill = self.score(as_hero: params[:as_hero], against_hero: params[:against_hero]) if params[:against_hero]
-    elsif params[:map] || params[:with_hero] || params[:against_hero]
-      min = 10
-      fill = self.score(as_hero: params[:as_hero])
-    else
-      min = 50
-      fill = 500
-    end
-
-    while scores.count < min
-      scores << fill
-    end
-
-    scores.sum / scores.count
-  end
-
   def pick_list(draft)
     list = self.roster.collect do |hero|
-      {hero: hero, score: self.score(as_hero: hero, draft: draft)}
+      {hero: hero, score: Scenario.new(user: self, as_hero: hero, draft: draft).score}
     end
 
     list.sort_by { |obj| obj[:score] }.reverse
@@ -123,31 +79,6 @@ class User < ApplicationRecord
   end
 
   private
-
-  def matches_as(hero)
-
-    self.tracked_matches.select do |match|
-      my_pick(match).hero == hero
-    end
-  end
-
-  def filter_by_map(matches, map)
-    matches.select do |match|
-      match.map == map
-    end
-  end
-
-  def filter_by_hero(matches, relationship, hero)
-    matches.select do |match|
-      match.hero_picks.find do |hero_pick|
-        hero_pick.hero == hero &&
-        (
-          (relationship == :with && hero_pick.team == my_team(match)) ||
-          (relationship == :against && hero_pick.team != my_team(match))
-        )
-      end
-    end
-  end
 
   def auto_heroes
     roster = self.tracked_matches.collect do |match|
