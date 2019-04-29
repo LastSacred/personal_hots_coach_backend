a6a1f31cc6d4cc84e14310983c273f6e813f59aa
Arthur Torres <arthurtorres002@gmail.com>
2019-04-29 11:15:33 -0400

    write test for user StatSet

--- test/models/stat_set.test.rb
+++ test/models/stat_set.test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StatSetTest < ActiveSupport::TestCase
 
-  test "new" do
+  test 'hero' do
     star = users :DavidBowie
     lili = heroes :LiLi
     stub_date = Date.new(2019, 3, 11)
@@ -15,6 +15,16 @@ class StatSetTest < ActiveSupport::TestCase
       assert_equal 540, statset.score
       assert_equal 83, statset.win_percent
       assert_equal 6, statset.match_count
+    end
+  end
+
+  test 'user' do
+    bob = users :BobRoss
+    stub_date = Date.new(2019, 3, 11)
+
+    Date.stub :today, stub_date do
+      statset = StatSet.new(bob)
+      assert_equal bob.heroes.count, statset.hero_sets.count
     end
   end
 
