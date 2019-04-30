4bfb2963f0e3109019b1857e53419fbdcdace3b1
Arthur Torres <arthurtorres002@gmail.com>
2019-04-30 09:52:04 -0400

    write test for statsets_controller

--- test/controllers/statsets_controller_test.rb
+++ test/controllers/statsets_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class StatsetsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "show" do
+    bob = User.find_by(name: "BobRoss")
+    token = JWT.encode({userId: bob.id}, ENV['SECRET'])
+
+    get statsets_url, headers: {
+      "Access-Token": token
+    }
+
+    assert_response :ok
+  end
 end
